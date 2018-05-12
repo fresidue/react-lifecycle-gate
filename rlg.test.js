bba7f3a1fa977b89f2ba612d704cac07aa842301
ext.frca <fresidue@gmail.com>
2018-05-12 15:58:14 +0200

    use jest

--- rlg.test.js
+++ rlg.test.js
@@ -2,13 +2,11 @@ import React from 'react';
 import {expect} from 'chai';
 import enzyme, {mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
-import {JSDOM} from 'jsdom';
 import {spy} from 'sinon';
 
 import LifecycleGate from './react-lifecycle-gate';
 
 enzyme.configure({adapter: new Adapter()});
-global.document = new JSDOM();
 spy(LifecycleGate.prototype, 'componentDidMount');
 
 describe('<LifecycleGate />', () => {
